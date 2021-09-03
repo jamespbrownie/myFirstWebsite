45b4d4fa976ed45548f29f7b5ee32ac914fb24c8
James Brownie <jamespbrownie@gmail.com>
2021-09-03 13:45:48 -0500

    added slideshow gallery

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
 
 const h2 = document.createElement('h2');
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "You are now entering my Web Page";
 
 document.querySelector("body").appendChild(h2);
+
+//Slideshow Gallery //
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+  if (n > slides.length) {slideIndex = 1} //restarts at 1 after the last slide
+  if (n < 1) {slideIndex = slides.length}
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+
+}
